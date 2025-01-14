776857693f88976d23b3955cf4347944a90d03e1
Oliver Hall <halloli@shernet.sheridancollege.ca>
2025-01-14 16:59:40 -0500

    coding gym updated

--- Assets/clockhourhand.cs
+++ Assets/clockhourhand.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class clockhourhand : MonoBehaviour
+public class ClockHourHand : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float rotationSpeed = 1f;
+
+
 
     // Update is called once per frame
     void Update()
-    { 
-        
+    {
+
+        {
+            transform.Rotate(Vector2.right, rotationSpeed * Time.deltaTime);
+
+        }
+
+
     }
+        
+    
 }
